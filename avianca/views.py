37649096e1f9aef4881e1fbb141e1a59048a9802
MiguelCock <macockc@eafit.edu.co>
2024-09-11 08:08:22 -0500

    planing views

--- avianca/views.py
+++ avianca/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, View, ListView
+from django import forms
 
 # Create your views here.
+
+
+class Hogar(TemplateView):
+    template_name="hogar.html"
+
+
+class Registrar():
+    template_name="registrar.html"
+
+
+class Lista(View):
+    template_name="lista.html"
+
+
+class Estadistica():
+    template_name="estadistica.html"
